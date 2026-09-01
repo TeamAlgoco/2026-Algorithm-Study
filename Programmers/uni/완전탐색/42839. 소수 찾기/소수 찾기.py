2bbe51e7fce3dfd6930adc2a4eea60630f57d496
AHNJIUN <AHNJIUN@users.noreply.github.com>
2026-09-01 01:20:42 +0000

    [Programmers-42839] \354\206\214\354\210\230\342\200\205\354\260\276\352\270\260

--- Programmers/uni/완전탐색/42839. 소수 찾기/소수 찾기.py
+++ Programmers/uni/완전탐색/42839. 소수 찾기/소수 찾기.py
@@ -1,18 +1,17 @@
 def solution(numbers):
     n = list(numbers)
-    cnt = 0
     lst = set()
-    def permutation(remain, path):
-        if path: 
-            lst.add(int("".join(path)))
+    def permutation(remain,path):
+        if path:
+            lst.add(int("".join(path)))        
         for i in range(len(remain)):
-            permutation(remain[:i]+remain[i+1:],path + [remain[i]])
+            permutation(remain[:i]+remain[i+1:], path + [remain[i]])
     def is_prime(x):
         if x < 2:
             return False
-        for j in range(2,x):
-            if x%j==0:
+        for s in range(2,x):
+            if x%s == 0 :
                 return False
         return True
-    permutation(n,[])
+    permutation(n, [])
     return len([x for x in lst if is_prime(x)])
