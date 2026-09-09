8697919f894dd0c16ac76b64193745d83fdc9e4c
AHNJIUN <AHNJIUN@users.noreply.github.com>
2026-09-09 04:54:37 +0000

    [Programmers-42584] \354\243\274\354\213\235\352\260\200\352\262\251

--- Programmers/uni/스택／큐/42584. 주식가격/주식가격.py
+++ Programmers/uni/스택／큐/42584. 주식가격/주식가격.py
@@ -1,16 +1,29 @@
+'''
 from collections import deque
 
 def solution(prices):
-    prices = deque(prices)
-    lst = []
-    while prices:
-        t = 0
-        p = prices.popleft()
-        for c in prices:
-            if p <= c:
-                t += 1
-            else : 
-                t += 1
-                break
-        lst.append(t)        
-    return lst
+  prices = deque(prices)
+  lst = []
+  while prices:
+      t = 0
+      p = prices.popleft()
+      for c in prices:
+          t += 1
+          if p > c:
+              break
+      lst.append(t)
+  return lst
+'''
+from collections import deque
+
+def solution(prices):
+    stack = deque([])
+    answer = [0]*len(prices)
+    for j,price in enumerate(prices):
+        while stack and price < prices[stack[-1]]:
+            answer[stack[-1]]= j - stack[-1]
+            stack.pop()
+        stack.append(j)
+    for i in stack:
+        answer[i] = len(prices)-i-1
+    return answer
