3a681b918413afb7965a468b4094fce8c5950526
AHNJIUN <AHNJIUN@users.noreply.github.com>
2026-09-10 14:23:43 +0000

    [Programmers-42748] K\353\262\210\354\247\270\354\210\230

--- Programmers/uni/정렬/42748. K번째수/K번째수.py
+++ Programmers/uni/정렬/42748. K번째수/K번째수.py
@@ -1,6 +1,10 @@
+'''
 def solution(array, commands):
-    answer = []
+    answer = [] 
     for start, end, k in commands:
         lst = sorted(array[start-1:end])
         answer.append(lst[k-1])
-    return answer
+    return answer         a
+'''
+def solution(array, commands):
+    return [sorted(array[start-1:end])[k-1] for start, end, k in commands]
