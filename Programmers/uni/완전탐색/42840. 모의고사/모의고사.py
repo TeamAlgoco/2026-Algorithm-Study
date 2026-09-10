0de7be88f9ab7abbf6f1f6864f37d3d6462eb11e
AHNJIUN <AHNJIUN@users.noreply.github.com>
2026-09-10 13:48:07 +0000

    [Programmers-42840] \353\252\250\354\235\230\352\263\240\354\202\254

--- Programmers/uni/완전탐색/42840. 모의고사/모의고사.py
+++ Programmers/uni/완전탐색/42840. 모의고사/모의고사.py
@@ -1,3 +1,4 @@
+'''
 def solution(answers):
     a = [1,2,3,4,5]
     b = [2,1,2,3,2,4,2,5]
@@ -15,3 +16,22 @@ def solution(answers):
         if lst[j] == m:
             answer.append(j+1)            
     return answer
+
+def solution(answers):
+    a = [1,2,3,4,5]
+    b = [2,1,2,3,2,4,2,5]
+    c = [3,3,1,1,2,2,4,4,5,5]
+    lst_abc = [a,b,c]
+    lst = [0]*len(lst_abc)
+    for i,x in enumerate(lst_abc):
+        for j,y in enumerate(answers):
+            if x[j%len(x)] == y:
+                lst[i] += 1
+    m = max(lst)
+    return [k+1 for k in range(len(lst)) if lst[k] == m]
+'''
+def solution(answers):
+    lst_abc = ([1,2,3,4,5],[2,1,2,3,2,4,2,5],[3,3,1,1,2,2,4,4,5,5])
+    lst=[sum(1 for j,y in enumerate(answers) if x[j%len(x)]==y) for x in lst_abc]
+    m = max(lst)
+    return [k+1 for k,z in enumerate(lst) if z == m]
