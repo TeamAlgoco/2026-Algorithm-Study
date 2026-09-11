2b067a735cfead4ada21d5ff28dc7a1d67cff7da
AHNJIUN <AHNJIUN@users.noreply.github.com>
2026-09-11 17:27:52 +0000

    [Programmers-43163] \353\213\250\354\226\264\342\200\205\353\263\200\355\231\230

--- Programmers/uni/깊이／너비 우선 탐색（DFS／BFS）/43163. 단어 변환/단어 변환.py
+++ Programmers/uni/깊이／너비 우선 탐색（DFS／BFS）/43163. 단어 변환/단어 변환.py
@@ -1,20 +1,33 @@
 from collections import deque
-
+'''
 def solution(begin, target, words):
-    q = deque([(begin,0)])
-    visited = [0]*len(words)
+    q = deque([(begin, 0)])
+    visited = [0] * len(words)
     while q:
-        x,answer = q.popleft()
+        x, answer = q.popleft()
         if x == target:
             return answer
         answer += 1
         for i in range(len(words)):
-            if visited[i]!=1:
+            if visited[i] != 1:
                 cnt = 0
                 for j in range(len(x)):
-                    if x[j]!=words[i][j]:
-                        cnt+=1
-                if cnt==1:
-                    q.append((words[i],answer))
+                    if x[j] != words[i][j]:
+                        cnt += 1
+                if cnt == 1:
+                    q.append((words[i], answer))
                     visited[i] = 1
-    return 0
+    return 0   
+'''
+def solution(begin, target, words):
+    q = deque([(begin,0)])
+    visited = set()
+    while q:
+        x,answer = q.popleft()
+        if x == target:
+            return answer
+        for w in words:
+            if w not in visited and sum(a!=b for a,b in zip(x,w))==1:
+                q.append((w,answer+1))
+                visited.add(w)
+    return 0 
